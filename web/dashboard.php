a8ee35c608fa2308f439d5336c32ff18f357e99f
Vincent Lau <vincent@paywhere.com>
2013-03-24 06:25:08 +0800

    Corrected view path
    Signed-off-by: Vincent Lau <vincent@paywhere.com>

--- web/dashboard.php
+++ web/dashboard.php
@@ -56,6 +56,9 @@ if ($_POST) {
 					case 'receive':
 						$getFile = 'receive-gift.php';
 						break;
+					case 'view':
+						$getFile = 'view-gift.php';
+						break;
 					default:
 						$getFile = 'profile.php';
 						break;
