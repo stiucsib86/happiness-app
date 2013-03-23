927f920aae3f1a4f44cda429eeae8ef59d1951bd
Vincent Lau <vincent@paywhere.com>
2013-03-24 03:00:53 +0800

    New updates
    Signed-off-by: Vincent Lau <vincent@paywhere.com>

--- web/dashboard.php
+++ web/dashboard.php
@@ -2,47 +2,45 @@
 $pageTitle = 'HappinessXchange - Dashboard';
 require_once('header.php');
 ?>
+<script>
 <?php
 if ($_POST) {
-	?>
-	<script>
-		jQuery.getJSON("http://happiness-app.ap01.aws.af.cm/user/update_user/?gender=<?php echo $_POST['gender']; ?>&dob=<?php echo $_POST['dob']; ?>&interest=<?php echo $_POST['interest']; ?>&about_me=<?php echo $_POST['about_me']; ?>&callback=?", function(data) {
-			console.log(data);
-		});
-	</script>
+	if($_GET['type']=='give' && $_POST['giftFlag']==1) { ?>
+	  jQuery.getJSON("http://happiness-app.ap01.aws.af.cm/gifting/send/?receiver_fb_id=<?php echo $_GET['id']; ?>&gifting_url=<?php echo urlencode($_POST['productURL']); ?>&callback=?", function(data) {
+		  console.log(data);
+		  alert('Congratulations, your friend has been notified!');
+	  });
+<?php
+	}
+	else if($_GET['type']=='receive' && $_POST['giftFlag']==1) { ?>
+	  jQuery.getJSON("http://happiness-app.ap01.aws.af.cm/gifting/accept/?gifting_id=<?php echo $_GET['gid']; ?>&thankyou_note=<?php echo urlencode($_POST['thankyou_note']); ?>&callback=?", function(data) {
+		  console.log(data);
+		  alert('Your friend is pleased that you like the gift!');
+	  });
+<?php
+	}
+	else { ?>
+	  jQuery.getJSON("http://happiness-app.ap01.aws.af.cm/user/update_user/?gender=<?php echo $_POST['gender']; ?>&dob=<?php echo $_POST['dob']; ?>&interest=<?php echo $_POST['interest']; ?>&about_me=<?php echo $_POST['about_me']; ?>&callback=?", function(data) {
+		  console.log(data);
+	  });
 	<?php
+	}
 }
 ?>
-
-<script>
-	jQuery.getJSON("http://happiness-app.ap01.aws.af.cm/user/?callback=?", function(data) {
-		console.log(data);
-		document.getElementById('profile-name').value = data.display_name;
-		document.getElementById('profile-email').value = data.email;
-		document.getElementById('profile-gender').value = data.gender;
-		document.getElementById('profile-dob').value = data.dob;
-		document.getElementById('profile-interests').value = data.interest;
-		document.getElementById('profile-interests').value = data.interest_text;
-		console.log(data.interest);
-		document.getElementById('profile-about').value = data.about_me;
-		if (data.username) {
-			document.getElementById("profileImg").src = 'https://graph.facebook.com/' + data.username + '/picture?type=large';
-			document.getElementById('fbProfileLink').innerHTML = '<a href="http://facebook.com/' + data.username + '/" target="_blank">View Facebook Profile</a>';
-		}
-	});
 </script>
-<div id="dashboard" class="container">
+
+<div class="dashboard container">
 	<div class="row-fluid top-item">
-
+    
 		<?php require_once 'header_mini.php'; ?>
-
+        
         <div class="row-fluid">
             <div class="span4 friendslist">
 				<?php require_once('friendlist.php'); ?>
             </div>
             <div class="span8">
 			<?php
-			switch(isset($_GET['type'])) {
+			switch($_GET['type']) {
 				case 'give':
             		$getFile = 'give-gift.php';
 				break;
