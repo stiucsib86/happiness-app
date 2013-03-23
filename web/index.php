25ea69d7425a11f1eeeeb8a15a72a41255f8ca07
Abhinit Ambastha <abhinit@berylstudio.com>
2013-03-24 00:45:22 +0800

    front page design

--- web/index.php
+++ web/index.php
@@ -2,31 +2,35 @@
 $pageTitle = 'HappinessXchange';
 require_once('header.php');
 ?>
-      <div class="maincontents container">
-        <div class="row">
-            <div class="logo"></div>
-        </div>
-        <div class="row">
-            <div class="span6 activities">
-                <h4>Recent Activities</h4>
-                <div class="row-fluid">
-                    <div class="span2"><img src="images/nouser.jpg"/></div>
-                    <div class="span8"><a href="#">Muthu</a> Gave <a href="#">xyzobject</a> to <a href="#">vincent</a> and recieved <a href="#">yyyobject</a></div>
-                    <div class="span2"><img src="images/nouser.jpg"/></div>
-                </div>
-                <div class="row-fluid">
-                    <div class="span2"><img src="images/nouser.jpg"/></div>
-                    <div class="span8"><a href="#">Muthu</a> Gave <a href="#">xyzobject</a> to <a href="#">vincent</a> and recieved <a href="#">yyyobject</a></div>
-                    <div class="span2"><img src="images/nouser.jpg"/></div>
-                </div>
-                <div class="row-fluid">                   
-                    <div class="span2"><img src="images/nouser.jpg"/></div>
-                    <div class="span8"><a href="#">Muthu</a> Gave <a href="#">xyzobject</a> to <a href="#">vincent</a> and recieved <a href="#">yyyobject</a></div>
-                    <div class="span2"><img src="images/nouser.jpg"/></div>
-                </div>
+      <div id="mainContent">
+
+            <div id="activities">
+                <div class="logo"></div>
+                <h4 id="activityHeading">Recent Activities</h4>
+                <ul>
+                    <li>
+                        <div class="fromImg"><img src="images/nouser.jpg"/></div>
+                        <div class="activityDesc"><a href="#">Muthu</a> Gave <a href="#">xyzobject</a> to <a href="#">vincent</a> and recieved <a href="#">yyyobject</a></div>
+                        <div class="toImg"><img src="images/nouser.jpg"/></div>
+                        <div class="clearfix"></div>
+                    </li>
+                    <li>
+                        <div class="fromImg"><img src="images/nouser.jpg"/></div>
+                        <div class="activityDesc"><a href="#">Muthu</a> Gave <a href="#">xyzobject</a> to <a href="#">vincent</a> and recieved <a href="#">yyyobject</a></div>
+                        <div class="toImg"><img src="images/nouser.jpg"/></div>
+                        <div class="clearfix"></div>
+                    </li>
+                    <li>                   
+                        <div class="fromImg"><img src="images/nouser.jpg"/></div>
+                        <div class="activityDesc"><a href="#">Muthu</a> Gave <a href="#">xyzobject</a> to <a href="#">vincent</a> and recieved <a href="#">yyyobject</a></div>
+                        <div class="toImg"><img src="images/nouser.jpg"/></div>
+                        <div class="clearfix"></div>
+                    </li>
+                </ul>
             </div>
-            <div class="span4 signup">
-                <h4>Help Spread the Happiness!</h4>
+
+            <div id="signup">
+                <h4 id="happiness">Help Spread the Happiness!</h4>
                 <h5>It's pretty simple really, here's how it works:</h5>
                 <div class="text-left howto">
                     <ol>
@@ -36,9 +40,9 @@ require_once('header.php');
                     </ol>
                     <p class="fineprint text-right">* p.s. your friend won't know it was you =)</p>
                 </div>
-                <div class="btn btn-large btn-primary fb-login-button" data-show-faces="false" data-width="200" data-max-rows="1" onclick="fb_login();"><i class=" icon-user icon-white"></i>&nbsp; Facebook Connect</div>
+                <div id="fbButton" class="btn btn-large btn-primary fb-login-button" data-show-faces="false" data-width="200" data-max-rows="1" onclick="fb_login();"><i class=" icon-user icon-white"></i>&nbsp; Facebook Connect</div>
             </div>
-        </div>
+            <div class="clearfix"></div>
       </div>
 <?php
 require_once('footer.php');
