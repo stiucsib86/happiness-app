32ef6745ceac7aafb81182f84ed470612c5f8b2f
Goh Bing Han <stiucsib86@hotmail.com>
2013-03-24 03:06:36 +0800

    Added MainAppCtrl.

--- web/header_mini.php
+++ web/header_mini.php
@@ -1,5 +1,5 @@
-<div class="row-fluid top-item" ng-app ng-cloak>
-	<div class="menu pull-right" ng-controller="MenuBarCtrl">
+<div class="row-fluid top-item">
+	<div class="menu pull-right">
 		<ul>
 			<li>
 				<div ng-show="getNumberUnreadNotifications() > 0">
@@ -22,57 +22,3 @@
 		<div class="logoLong"></div>
 	</div>
 </div>
-
-<style>
-	[ng\:cloak], [ng-cloak], [data-ng-cloak], [x-ng-cloak], .ng-cloak, .x-ng-cloak {
-		display: none;
-	}
-</style>
-<script>
-	function MenuBarCtrl($scope, $rootScope, $http) {
-
-		$rootScope.user = {};
-
-		$rootScope.notifications = [];
-		$rootScope.notifications._loading = true;
-
-		$rootScope.getNumberUnreadNotifications = function() {
-			var _count = 0;
-			angular.forEach($rootScope.notifications, function(notification, key) {
-				if (notification.is_read == 0) {
-					_count++;
-				}
-			});
-			return _count;
-		};
-
-		$rootScope.get_all_notifications = function() {
-			jQuery.getJSON('<?php echo API_ENDPOINT ?>/notifications/?callback=?', {
-			}, function(xhrResponse) {
-				$scope.$apply(function() {
-					$rootScope.notifications._loading = false;
-					$rootScope.notifications = xhrResponse;
-					console.log('$rootScope.notifications', $rootScope.notifications);
-				});
-			});
-		};
-
-		$rootScope.get_user_data = function() {
-			jQuery.getJSON('<?php echo API_ENDPOINT ?>/auth/?callback=?', {
-			}, function(xhrResponse) {
-				$scope.$apply(function() {
-					$rootScope.user._loading = false;
-					$rootScope.user = xhrResponse;
-					console.log('$rootScope.user', $rootScope.user);
-				});
-			});
-		};
-
-		(function() {
-			// Initialize
-			$rootScope.get_all_notifications();
-			$rootScope.get_user_data();
-		})();
-
-	}
-</script>
