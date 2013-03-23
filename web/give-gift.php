32038ff52794a6ce4eadb2df8d95ff6b3f0c94af
Vincent Lau <vincent@paywhere.com>
2013-03-24 01:36:58 +0800

    Added Scraper to Gifting
    Signed-off-by: Vincent Lau <vincent@paywhere.com>

--- web/give-gift.php
+++ web/give-gift.php
@@ -1,35 +1,70 @@
                 <h4>Gifting</h4>
-                
-                <div class="row-fluid" style="margin-bottom:20px">
-                    <div class="span12 tips">
-                        <span class="label">Tips</span> Copy andd <strong>Paste</strong> the product URL that you wish to give as gift. Otherwise, the product will <u>not</u> not be send to your friend.<a href="http://www.tackthis.com/shop/20/">http://www.tackthis.com/shop/20/</a>
-                    </div>
-                </div>
+
                 <div class="row-fluid"> 
                     <div class="span12">
-                        <form class="form-inline text-center">
-                            <input class="span10" type="text" placeholder="URL of the Product"><a href="#">&nbsp; ?</a><br/>
-                            
-                              
-                           
-                          </form>
+                        <form class="form-inline" method="post" action="/dashboard/give-gift/<?php if(isset($_GET['id'])) echo $_GET['id']; ?>" target="_self">
+                            <input class="span10" type="text" name="productURL" id="productURL" placeholder="http://www.amazon.com/gp/product/B007OZNZG0/" value="<?php if(isset($_POST['productURL'])) echo $_POST['productURL']; ?>"> 
+                            <button class="btn btn-primary">Create Gift</button>
+                        </form>
+                        <div class="span12 tips">
+                            <span class="label">Tips</span> Copy and paste the URL of the product website</a>
+                        </div>
                     </div>
-                </div> 
+                </div><?php
+				include_once('/plugins/simplehtmldom_1.5/simple_html_dom.php');
+				
+				function scraping_shop($url) {
+					// create HTML DOM
+					$html = file_get_html($url);
+				
+					// find all image
+					foreach($html->find('img') as $e) {
+					  if(!preg_match("/g-ecx./i", $e->src)) {
+						$scrappedData['Image'] = $e->src;
+						break;
+					  }
+					}
+				
+					// find all span tags with class=gb1
+					foreach($html->find('span#btAsinTitle') as $e) {
+						$scrappedData['Product'] = $e->innertext;
+						break;
+					}
+				
+					// find all span tags with class=gb1
+					foreach($html->find('span.s_star_4_5') as $e) {
+						$scrappedData['Rating'] = strip_tags($e->innertext);
+						break;
+					}
+
+					// find all span tags with class=gb1
+					foreach($html->find('span.price') as $e) {
+						$scrappedData['Price'] = strip_tags($e->innertext);
+						break;
+					}
+
+					// clean up memory
+					$html->clear();
+					unset($html);
+				
+					return $scrappedData;
+				}
+				
+				if($_POST) {
+					$scrappedData = scraping_shop($_POST['productURL']); ?>
                 <div class="row-fluid">
                     <div class="well">
-                        <div class="row-fluid">
-                        <div class="span4"><img class="giftbox" src="/images/tshirt.jpg"/></div>
-                        <div class="span3">
-                            <h5>Lego T-shirt</h5>
-                            <h5>S$20.00</h5>
-                            <h5><img style="max-width:100px;" src="/images/rating.jpg"/></h5>
+                       <div class="row-fluid">
+                        <div class="span4"><img class="giftbox" src="<?php echo $scrappedData['Image']; ?>"/></div>
+                        <div class="span8">
+                            <h5><?php echo $scrappedData['Product']; ?></h5>
+                            <h5><?php echo $scrappedData['Price']; ?></h5>
+                            <h5>Rated <?php echo $scrappedData['Rating']; ?></h5>
                         </div>
-                        <div class="span5">
-                            Matching is where we take all the participants in an exchange, put them into a big hat, shake the hat up and start exchanging names. It's a bit more complicated than that, but you get the information of another person to send a gift to.
-                        </div>
-                        </div>
+                       </div>
                     </div>
                 </div>
                 <div class="row-fluid text-center">
-                    <button class="btn btn-large btn-primary" type="button"><i class="icon-white icon-gift"></i>&nbsp; Give It as Gift</button>
-                </div>
+                    <button class="btn btn-large btn-primary" onClick="giveGift();"><i class="icon-white icon-gift"></i>&nbsp; Give this Gift</button>
+                </div><?php
+				} ?>
